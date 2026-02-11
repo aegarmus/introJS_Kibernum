fd79c8a0557373143dcd77784c4f0792a1aaabe8
Alan Garcia <aegarmus@gmail.com>
2026-02-11 20:15:22 -0300

    feat: variable explanation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
 console.log('Estamos conectados a Javascript!!')
+
+// Variables
+
+const numero1 = 3.1412 // -> Almacena en memoria valores "constantes" -> que no puede cambiar su valor de referencia ✅
+
+let numero2 = 27 // -> almacena en memoria valores que pueden ser modificados o reasignando su valor de referencia ✅
+
+var numero3 = 15 // -> Almacena en memoria valores que puden ser modificados o reasignada su referencia pero actua de forma global ❌
+
+/* Scope 
+
+Hace referencia a el alcance que tiene un bloque de código { }
+
+function nombreFuncion (parameto) {
+    desde aqui
+    |
+    |
+    |
+    hasta aqui llega el scope de la función
+}
+
+Var es global, esto quiere decir que actua sobr toooooodo el código
+Let es de scope local, esto quiere decir que solo existe dentro del bloque de código en que es creado
+
+
+*/
+
+
+function mostrarNumero () {
+    let numero = 5
+    console.log(numero)
+}
+
+console.log(numero)
