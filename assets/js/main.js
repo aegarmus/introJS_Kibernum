4b7bf82bdbea153e670857e82d5aba91c1e1d302
Alan Garcia <aegarmus@gmail.com>
2026-02-11 21:19:11 -0300

    feat: prmitive data types examples

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,62 @@ let saludo = (nombre) => {
 }
 
 console.log(saludo(nombre))
+
+
+
+// Tipos de datos
+
+/*
+-> Primitivos -> Son los tipos de datos que tienen su propio prototipo de referencias -> Alberga valores con un único valor asociado
+
+
+-> Referenciales
+*/
+
+//Primitivos
+//Strings
+let string = 'hola soy una cadena de texto' // Cadenas de caracteres, pueden tener números, letras, simbolos especiales siempre entre comillas
+
+//Numbers
+let enteros = 456 // Este es un número entero positivo
+let flotantes = 5.456 // Este es un número flotante positivo
+
+let negativo = -546
+let flotanteNegativo = -6.542
+
+let infinito = Infinity // infinito positivo
+let infinitoNegativo = -Infinity // infinito negativo
+
+let notANumber = NaN // Valor número especial que indica que algo NO ES UN NÚMERO
+
+// Booleans
+let verdadero = true
+let falso = false
+
+// null -> Este es ESPECIAL -> hace alución a un valor vacío o inexistente
+
+let nulo = null
+
+// undefined -> Aparece cuando la variable es declarada pero no se le asigna un valor
+
+let indefinido
+
+console.log(indefinido)
+
+// BigInt -> Hace referencia a valores con nnotación cientifica
+
+let bigInt = 5.45621e-21
+
+console.log(bigInt)
+
+//Symbol -> Se usa para valores con una referencia unica
+
+let letra1 = 'a'
+let letra2 = 'a'
+let letra3 = Symbol('a')
+
+console.log(letra1)
+console.log(letra2)
+console.log(letra3)
+
+console.log(letra1 == letra3)
